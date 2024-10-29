9f3128795ab5ddc19649912663d9d7306b43935b
Giovanni <gvn.gentile@gmail.com>
2024-10-28 21:53:50 -0300

    chore: optimize imports

--- src/main/java/com/giovicente/application/PERTApplication.java
+++ src/main/java/com/giovicente/application/PERTApplication.java
@@ -1,13 +1,6 @@
 package com.giovicente.application;
 
-import com.giovicente.pert.PERTCalculator;
 import com.giovicente.pert.PERTMenu;
-import com.giovicente.utils.Printer;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class PERTApplication {
     public static void main(String[] args) {
