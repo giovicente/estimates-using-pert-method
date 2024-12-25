ca1cccf8258ede88e141f0cc4ade5a076077c7dd
Giovanni <gvn.gentile@gmail.com>
2024-12-25 17:21:12 -0300

    test: increasing PERTMenu tests coverage

--- src/test/java/com/giovicente/controller/tests/PERTMenuTest.java
+++ src/test/java/com/giovicente/controller/tests/PERTMenuTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -22,6 +24,8 @@ class PERTMenuTest {
     @Mock
     private Scanner mockScanner;
 
+    private String simulatedInputs;
+
     @Test
     void shouldThrowIllegalStateExceptionWhenAttemptingToInstantiate() throws NoSuchMethodException {
         Constructor<PERTMenu> constructor = PERTMenu.class.getDeclaredConstructor();
@@ -29,7 +33,7 @@ class PERTMenuTest {
 
         Exception exception = assertThrows(InvocationTargetException.class, constructor::newInstance);
 
-        assertTrue(exception.getCause() instanceof IllegalStateException);
+        assertInstanceOf(IllegalStateException.class, exception.getCause());
         assertEquals("This class can't be instantiated", exception.getCause().getMessage());
     }
 
@@ -51,22 +55,117 @@ class PERTMenuTest {
         System.setOut(new PrintStream(outputStream));
 
         when(mockScanner.next())
-                .thenReturn(String.valueOf('A'))
-                .thenReturn(String.valueOf('Y'));
+                .thenReturn(String.valueOf('A')) // Invalid input continuity value
+                .thenReturn(String.valueOf('Y')); // Valid input continuity value
 
         PERTMenu.hasContinuity(mockScanner);
 
         System.setOut(System.out);
 
         String actualPrintedOutput = outputStream.toString();
-        String expectedPrintedOutput
-                = "Do you want to do another estimate? Type Y or N: " +
-                "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \r\n" +
-                "Invalid Option! It must be Y or N!!!\r\n" +
-                "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \r\n" +
-                "Do you want to do another estimate? Type Y or N: " +
-                "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \r\n";
 
-        Assertions.assertEquals(expectedPrintedOutput, actualPrintedOutput);
+        assertTrue(actualPrintedOutput.contains("Do you want to do another estimate? Type Y or N: "));
+        assertTrue(actualPrintedOutput.contains("Invalid Option! It must be Y or N!!!"));
+    }
+
+    @Test
+    void shouldReturnInvalidOptionMessageForIndividualEstimate() {
+        /*
+        * Simulated inputs for runPERT() method:
+        * . 0 is an invalid option for selecting either an individual (1) or batch (2) estimate
+        * . 1 is a valid option for selecting an individual estimate
+        * . 3 is the optimistic estimate
+        * . 5 is the nominal estimate
+        * . 11 is the pessimistic estimate
+        * . N indicates that the user does not want to make a new estimate
+        * */
+        simulatedInputs = "0\n1\n3\n5\n11\nN";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInputs.getBytes());
+
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream actualOutput = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            PERTMenu.runPERT();
+        } finally {
+            System.setOut(actualOutput);
+            System.setIn(System.in);
+        }
+
+        String capturedOutput = outputStream.toString();
+
+        assertTrue(capturedOutput.contains("Invalid Option! It must be 1 or 2!!!"));
+    }
+
+    @Test
+    void shouldReturnInvalidOptionMessageForBatchEstimate() {
+        /*
+         * Simulated inputs for runPERT() method:
+         * . 2 is a valid option for selecting a batch estimate
+         * . 1 is the optimistic estimate
+         * . 3 is the nominal estimate
+         * . 5 is the pessimistic estimate
+         * . A is an invalid option for the question about add a new estimate to the list
+         * . First N indicates that the user does not want to add a new estimate to the list
+         * . B is an invalid option for the question about whether to make a new estimate
+         * . Second N indicates that the user does not want to make a new estimate
+         * */
+        simulatedInputs = "2\n1\n3\n5\nA\nN\nB\nN";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInputs.getBytes());
+
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream actualOutput = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            PERTMenu.runPERT();
+        } finally {
+            System.setOut(actualOutput);
+            System.setIn(System.in);
+        }
+
+        String capturedOutput = outputStream.toString();
+
+        assertTrue(capturedOutput.contains("Invalid Option! It must be Y or N!!!"));
+    }
+
+    @Test
+    void shouldReturnCorrectFinalEstimations() {
+        /*
+         * Simulated inputs for runPERT() method:
+         * . 2 is a valid option for selecting a batch estimate
+         * . 1 is the optimistic estimate
+         * . 3 is the nominal estimate
+         * . 5 is the pessimistic estimate
+         * . First N indicates that the user does not want to add a new estimate to the list
+         * . Second N indicates that the user does not want to make a new estimate
+         * */
+        simulatedInputs = "2\n1\n3\n5\nN\nN";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInputs.getBytes());
+
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream actualOutput = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            PERTMenu.runPERT();
+        } finally {
+            System.setOut(actualOutput);
+            System.setIn(System.in);
+        }
+
+        String capturedOutput = outputStream.toString();
+
+        assertTrue(capturedOutput.contains("Task Sequence Duration: 3.0"));
+        assertTrue(capturedOutput.contains("Task Sequence Standard Deviation: 0.7"));
+        assertTrue(capturedOutput.contains("Final Estimate: 3.7"));
+        assertTrue(capturedOutput.contains("Final Estimate (Worst Case Scenario): 4.4"));
     }
 }
